108416c741b3af3a4d3d2f4f07e376392ce8e1df
FlingJLJ <60308125+FlingJLJ@users.noreply.github.com>
2020-08-06 20:58:21 -0500

    Initial commit

---
+++ src/main.lua
@@ -0,0 +1,17 @@
+local money = 0
+local dps = 1
+
+while true do -- main loop
+  for _ = 1, 10 do
+    os.execute("cls")
+    
+    print("dps: " .. dps)
+    print("money: " .. money)
+    
+    io.read()
+    
+    money = money + 1
+  end
+  
+  dps = dps + 1
+end
