daad0060df982cf16982abbcd58c544faef7f880
FlingJLJ <60308125+FlingJLJ@users.noreply.github.com>
2020-11-01 08:52:00 -0600

    e

--- src/main.lua
+++ src/main.lua
@@ -3,7 +3,8 @@ local dps = 1
 
 while true do -- main loop
   for _ = 1, 10 do
-    os.execute("cls")
+    os.execute("clear > nul")
+    os.execute("cls > nul")
     
     print("dps: " .. dps)
     print("money: " .. money)
